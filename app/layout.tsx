9b83e2d5b56f527eabc509792c65c4ed5628351f
Khalid <khalid.echchahid@usmba.ac.ma>
2025-02-15 20:27:20 +0100

    new facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1725231501604996');
+          fbq('init', '603940898926819');
           fbq('track', 'PageView');
         `}
         </Script>
@@ -61,7 +61,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=1725231501604996&ev=PageView&noscript=1`}
+            src={`https://www.facebook.com/tr?id=603940898926819&ev=PageView&noscript=1`}
           />
         </noscript>
       </body>
